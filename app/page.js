ded67436baed97b6f6c878726992658adf348cf6
ilkay <06ilkayozturk@gmail.com>
2024-03-21 21:15:44 +0300

    created the header section

--- app/page.js
+++ app/page.js
@@ -1,10 +1,3 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
-  return (
-    <div>
-      <h1>ialdakd</h1>
-      <Button>Submit</Button>
-    </div>
-  );
+  return <div>Home</div>;
 }
