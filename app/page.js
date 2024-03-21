d2c665fd1e5cc75e12997b6ca149491b5b1cfc21
ilkay <06ilkayozturk@gmail.com>
2024-03-21 19:58:13 +0300

    created the project setup

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>ialdakd</h1>
+      <Button>Submit</Button>
+    </div>
+  );
+}
